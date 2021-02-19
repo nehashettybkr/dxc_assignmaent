d0e21975793aed9dd40b21fcd4034f83d571dd23
nehashettybkr <36448585+nehashettybkr@users.noreply.github.com>
2021-02-18 20:10:15 -0800

    Add files via upload

---
+++ PythonModules.py
@@ -0,0 +1,7 @@
+import os
+print(os.name)
+print(os.path)
+print(os.getcwd())
+print(os.chdir('C:\\Users\\Acer\\PycharmProjects\\Python language\\PythonScripting'))
+print(os.getcwd())
+print(dir(os))
